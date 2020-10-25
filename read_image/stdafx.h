693924e0a1e6809f3244658e3ec3fd9bcbcac3db
chunxu <chunxuwu_zjut@163.com>
2020-10-25 14:45:50 +0800

    添加项目文件。

---
+++ read_image/stdafx.h
@@ -0,0 +1,12 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <cstdio>
+
+#include <imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+#include"watershed_test.h"
---
+++ read_image/watershed_test.h
@@ -0,0 +1,9 @@
+#pragma once
+#include"stdafx.h"
+
+class watershed_test
+{
+public:
+	int my_watershed(int argc, char* argv[]);
+};
+
